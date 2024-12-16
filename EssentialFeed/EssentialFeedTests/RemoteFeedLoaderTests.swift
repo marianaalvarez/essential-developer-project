57dfe2bdbb9c34556877c2fabc66ed46aa79d355
Mariana Alvarez de Carvalho <marialvarez@Marianas-MacBook-Pro.local>
2024-12-16 18:20:13 -0300

    RemoteFeedLoader does not request data during the init

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Mariana Alvarez de Carvalho on 16/12/2024.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+
+        XCTAssertNil(client.requestedURL)
+    }
+}
