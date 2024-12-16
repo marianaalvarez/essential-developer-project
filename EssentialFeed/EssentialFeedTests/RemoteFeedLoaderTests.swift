cb58250fe4e2e2723b5cb414f8c83beef3060d20
Mariana Alvarez de Carvalho <marialvarez@Marianas-MacBook-Pro.local>
2024-12-16 18:48:20 -0300

    Add URL parameter to RemoteFeedLoader

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,13 +9,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -35,17 +37,19 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-url.com")!
+        _ = RemoteFeedLoader(client: client, url: url)
 
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_RequestDataFromURL() {
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-url.com")!
+        let sut = RemoteFeedLoader(client: client, url: url)
         
         sut.load()
 
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
