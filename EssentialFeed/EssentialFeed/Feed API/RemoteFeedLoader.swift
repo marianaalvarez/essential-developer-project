b8f60b01785f073f71c23ae75c833746b3e570d2
Mariana Alvarez de Carvalho <marialvarez@Marianas-MacBook-Pro.local>
2024-12-20 15:16:20 -0300

    Move RemoteFeedLoader to production

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Mariana Alvarez de Carvalho on 20/12/2024.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
