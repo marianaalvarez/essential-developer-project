d7e052c624ae5a3c5becd6b66c8b67fa2e8d1a58
Mariana Alvarez de Carvalho <marialvarez@Marianas-MacBook-Pro.local>
2025-02-24 19:53:50 -0300

    Extract FeedCachePolicy to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,26 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Mariana Alvarez de Carvalho on 24/2/2025.
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        
+        return date < maxCacheAge
+    }
+}
